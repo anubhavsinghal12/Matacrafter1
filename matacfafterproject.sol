5ae50114c919c9d85697dc75a5ec073f4c8798fd
anubhavsinghal12 <108897323+anubhavsinghal12@users.noreply.github.com>
2024-07-11 12:28:46 +0530

    Update matacfafterproject.sol

--- matacfafterproject.sol
+++ matacfafterproject.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 contract Example {
     address public owner;
     uint public balance;
+    mapping(address => uint) public deposits;
 
     constructor() {
         owner = msg.sender;
@@ -17,25 +18,32 @@ contract Example {
     function deposit(uint amount) public onlyOwner {
         require(amount > 0, "Deposit amount must be greater than zero");
         balance += amount;
+        deposits[msg.sender] += amount;
     }
 
     function withdraw(uint amount) public onlyOwner {
         require(amount <= balance, "Insufficient balance");
-        uint oldBalance = balance;
+        uint previousBalance = balance;
         balance -= amount;
-        assert(balance == oldBalance - amount);
+        assert(balance == previousBalance - amount);
+        deposits[msg.sender] -= amount;
     }
 
     function emergencyWithdraw() public onlyOwner {
-        if (balance == 0) {
-            revert("Balance is already zero, nothing to withdraw");
-        }
-    
+        require(balance > 0, "Balance is already zero, nothing to withdraw");
         balance = 0;
-      
     }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        owner = newOwner;
+    }
+
+    function ownerDepositBalance() public view returns (uint) {
+        return deposits[owner];
+    }
+
     receive() external payable {
         balance += msg.value;
     }
 }
-
